6d883e6f1a6dc7ac58eb47f130cf9ef338e30b1d
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-27 08:43:47 +0000

    cs

--- models/summary/common_aggregations.sql
+++ models/summary/common_aggregations.sql
@@ -3,9 +3,9 @@
 {{ config(materialized='view') }}
 
 -- Use the common_aggregations macro for the 'clean_sales' table and the 'amount' column
-SELECT * FROM {{ common_aggregations('data-pipeline-project-445905.dbt_vkv.clean_sales', 'amount', 'month') }}
+{{ common_aggregations('data-pipeline-project-445905.dbt_vkv.clean_sales', 'amount', 'month') }}
 
-UNION ALL
+-- UNION ALL
 
--- Use the same macro for the 'clean_products' table and the 'price' column
-SELECT * FROM {{ common_aggregations('data-pipeline-project-445905.dbt_vkv.clean_products', 'price', 'quarter') }}
+-- -- Use the same macro for the 'clean_products' table and the 'price' column
+-- SELECT * FROM {{ common_aggregations('data-pipeline-project-445905.dbt_vkv.clean_products', 'price', 'quarter') }}
