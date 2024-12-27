f8fcbc4d89e62034f6b43c150ebe5b4cd431acdf
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-27 05:41:15 +0000

    cs

--- models/summary/sales_summary.sql
+++ models/summary/sales_summary.sql
@@ -1,3 +1,9 @@
+-- models/sales_summary.sql
+
+{{ config(
+    materialized='table'
+) }}
+
 WITH monthly_sales AS (
     SELECT
         -- Extracting the year and month from the sale_date
@@ -5,7 +11,7 @@ WITH monthly_sales AS (
         products.category,
         COUNT(sales.sale_id) AS total_sales, -- Using the count of sales IDs as total sales
         SUM(products.price) AS total_revenue -- Summing up prices for revenue calculation
-    FROM `data-pipeline-project-445905`.`dbt_vkv`.`clean_sales` AS sales
+    FROM `data-pipeline-project-445905`.`big_query_id`.`raw_sales` AS sales
     JOIN `data-pipeline-project-445905`.`dbt_vkv`.`clean_products` AS products
         ON sales.product_id = products.product_id
     GROUP BY
