93dc51e31b43a17428e0d77aae2563c687bae50b
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-27 07:36:07 +0000

    cs

--- models/summary/common_aggregations.sql
+++ models/summary/common_aggregations.sql
@@ -3,5 +3,5 @@
 {{ config(materialized='view') }}
 
 -- Correctly call the macro without backticks around the table name
-{{ common_aggregations('data-pipeline-project-445905.dbt_vkv.clean_sales', 'amount') }}
+{{ common_aggregations('data-pipeline-project-445905.dbt_vkv.clean_products', 'price') }}
 
