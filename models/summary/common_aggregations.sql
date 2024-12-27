2f1e0ccfa83acece68f9473f5c1ca347e8c54f8f
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-27 09:11:49 +0000

    cs

--- models/summary/common_aggregations.sql
+++ models/summary/common_aggregations.sql
@@ -3,8 +3,8 @@
 -- Use the macro for 'clean_sales' table, 'amount' column, and 'MONTH' as time period (default)
 {{ common_aggregations('clean_sales', 'amount') }}
 
--- Use the macro for 'clean_products' table, 'price' column, and 'QUARTER' as time period
-{{ common_aggregations('clean_products', 'price', 'QUARTER') }}
+-- -- Use the macro for 'clean_products' table, 'price' column, and 'QUARTER' as time period
+-- {{ common_aggregations('clean_products', 'price', 'QUARTER') }}
 
--- Use the macro for 'clean_sales' table, 'amount' column, and 'YEAR' as time period
-{{ common_aggregations('clean_sales', 'amount', 'YEAR') }}
+-- -- Use the macro for 'clean_sales' table, 'amount' column, and 'YEAR' as time period
+-- {{ common_aggregations('clean_sales', 'amount', 'YEAR') }}
