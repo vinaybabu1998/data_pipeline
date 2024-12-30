a68193ad2f708bafcc53b36a2a0707c921b733c1
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-30 08:50:20 +0000

    cs

--- models/summary/common_aggregations.sql
+++ models/summary/common_aggregations.sql
@@ -1,5 +1,5 @@
 WITH aggregated_data AS (
-    {{ common_aggregations('price', 'sale_date', '2024-01-01', '2024-12-31', 'monthly') }}
+    {{ calculate_aggregations('price', 'sale_date', '2024-01-01', '2024-12-31', 'monthly') }}
 )
 
 SELECT * FROM aggregated_data;
