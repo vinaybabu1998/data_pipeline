6c50291a08a671e195f3815009a6767665469f03
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-30 08:49:08 +0000

    cs

--- macros/common_aggregations.sql
+++ macros/common_aggregations.sql
@@ -1,4 +1,4 @@
-{% macro common_aggregations(numeric_column, date_column, start_date, end_date, time_period='monthly') %}
+{% macro calculate_aggregations(numeric_column, date_column, start_date, end_date, time_period='monthly') %}
     WITH data AS (
         SELECT
             {{ numeric_column }},
@@ -20,7 +20,9 @@
         
         SUM({{ numeric_column }}) AS total_value,
         AVG({{ numeric_column }}) AS average_value,
-        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY {{ numeric_column }}) AS median_value
+        
+        -- Use APPROX_QUANTILES for median calculation in BigQuery
+        APPROX_QUANTILES({{ numeric_column }}, 2)[OFFSET(1)] AS median_value
     FROM
         data
     GROUP BY
