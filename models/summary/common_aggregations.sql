2b4bedac1feb28943f105fd6bd590d5da901359a
vinaybabushiva@gmail.com <vinaybabushiva@gmail.com>
2024-12-30 09:08:37 +0000

    cs

--- models/summary/common_aggregations.sql
+++ models/summary/common_aggregations.sql
@@ -6,25 +6,3 @@
     period='month'
   ) 
 }}
-
-UNION ALL
-
-{{
-  common_aggregations(
-    table_name='`data-pipeline-project-445905.dbt_vkv.clean_products`',
-    date_column='sale_date',
-    numeric_column='price',
-    period='quarter'
-  )
-}}
-
-UNION ALL
-
-{{
-  common_aggregations(
-    table_name='`data-pipeline-project-445905.dbt_vkv.clean_sales`',
-    date_column='sale_date',
-    numeric_column='amount',
-    period='year'
-  )
-}}
